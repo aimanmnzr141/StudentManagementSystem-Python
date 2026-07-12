16ceaf3f9ad84819c9c857ddff85714f19d03525
aimanmnzr141 <aimanmanzoor141@gmail.com>
2026-07-12 20:08:42 +0530

    Created Main.py file

---
+++ main.py
@@ -0,0 +1,38 @@
+# From manager.py, class StudentManager is being imported
+from manager import StudentManager
+
+# Created an instance/object of class StudentManager
+manager = StudentManager()
+
+while True:
+    print("\n---------------Welcome To Student Management System---------------")
+    print("1. Add Student")
+    print("2. View Student")
+    print("3. Search Student")
+    print("4. Update Student")
+    print("5. Delete Student")
+    print("6. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        manager.add_student()
+
+    elif choice == 2:
+        manager.view_students()
+
+    elif choice == 3:
+        manager.search_student()
+
+    elif choice == 4:
+        manager.update_student()
+
+    elif choice == 5:
+        manager.delete_student()
+
+    elif choice == 6:
+        print("Thank you for using Student Management System")
+        break
+
+    else:
+        print("Invalid Input Occured! Please write valid input")
--- manager.py
+++ manager.py
@@ -53,14 +53,15 @@ class StudentManager:
 
         print("Sorry! Student not found")
 
-    def update_student(self) # Defining update method
+    def update_student(self):  # Defining update method
         # Asking user to enter the student id they want to update.
-        update_id = int(input("Enter student id you want to update:")) # input stored in variable update_id
+        # input stored in variable update_id
+        update_id = int(input("Enter student id you want to update:"))
         # Loop through self.students
-        for update_student in self.students:  
-            if update_student.student_id == update_id: # Comparing the id
+        for update_student in self.students:
+            if update_student.student_id == update_id:  # Comparing the id
                 print("Student Found!")
-                update_student.student_id = int(input("Enter New id:")) # Taking input from user and updating accordingly
+                # Taking input from user and updating accordingly
                 update_student.name = input("Enter New Name: ")
                 update_student.age = int(input("Enter New Age: "))
                 update_student.course = input("Enter New Course : ")
@@ -69,16 +70,13 @@ class StudentManager:
                 return
         print("Sorry! Student not found")
 
-    def delete_student(self): # Defining Delete Method
+    def delete_student(self):  # Defining Delete Method
         # Getting student id that user wants to delete
         delete_id = int(input("Enter id that you want to delete :"))
         for del_student in self.students:
-            if del_student.student_id == delete_id: #Compare each students ID with delete_id
+            # Compare each students ID with delete_id
+            if del_student.student_id == delete_id:
                 self.students.remove(del_student)
                 print("Student Deleted Successfully!")
                 return
         print("Sorry! Student not found")
-
-
-
-
